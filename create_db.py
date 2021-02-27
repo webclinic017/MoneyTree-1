fd389011506847f5d2c56ec22850295a00ae5551
Kyle Springfield <kspringfield13@gmail.com>
2021-02-27 14:42:40 -0500

    started on backtesting

--- create_db.py
+++ create_db.py
@@ -33,6 +33,20 @@ cursor.execute("""
     )
 """)
 
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_price_minute (
+        id INTEGER PRIMARY KEY,
+        stock_id INTEGER,
+        datetime NOT NULL,
+        open NOT NULL,
+        high NOT NULL,
+        low NOT NULL,
+        close NOT NULL,
+        volume NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+""")
+
 # insert into strategy table. (eventually build into MT app)
 # INSERT INTO strategy (name) values (<new strategy name>);
 
